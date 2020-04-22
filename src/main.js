a1ad44c919458d000dfe8a9223fd4eae9b433cf4
Joaquim Perez <jivkim@gmail.com>
2020-04-22 10:31:49 +0200

    error message if IE

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// https://stackoverflow.com/a/21712356/8345160
+if (window.document.documentMode) {
+  document.getElementById("app").innerHTML = "Le jeu n'est pas compatible avec votre navigateur, déso"
+  throw new Error("IE")
+}
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
