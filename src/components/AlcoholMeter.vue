8c7842f686e542b3e74146b413a02884c3d3d462
Joaquim Perez <jivkim@gmail.com>
2021-06-07 23:00:54 +0200

    don't shake alcohol meter's hand if game is over

--- src/components/AlcoholMeter.vue
+++ src/components/AlcoholMeter.vue
@@ -14,12 +14,13 @@
         transform: `rotate(${degrees}deg)`,
         transition: 'transform 2s'
       }">
-        <g id="hand-inner" :class="`shake-${$store.getters.drunkLevel}`">
+        <g id="hand-inner" :class="`shake-${shake}`">
           <circle cx="50" cy="50" r="6"/>
           <line x1="50" y1="50" x2="10" y2="50" stroke="white" stroke-width="2" />
         </g>
       </g>
-      <text x="50" y="25" fill="white" font-size="30px" dominant-baseline="middle" text-anchor="middle">%.</text>
+      <text x="50" y="25" fill="white" font-size="30px" dominant-baseline="middle" text-anchor="middle">
+        {{ $store.getters.drunkLevel }} %.</text>
       <text x="2" y="48" fill="white">0</text>
       <text x="98" y="48" fill="white" text-anchor="end">12</text>
     </svg>
@@ -35,6 +36,9 @@ export default {
     degrees() {
       let margin = 1
       return margin + (180 - 2 * margin) * this.drunkRatio
+    },
+    shake() {
+      return this.$store.state.over ? 0 : this.$store.getters.drunkLevel
     }
   }
 }
@@ -54,18 +58,17 @@ svg {
   to { transform: rotate(-1deg) }
 }
 @keyframes shake-2 {
-  from { transform: rotate(2deg) }
-  to { transform: rotate(-2deg) }
+  from { transform: rotate(3deg) }
+  to { transform: rotate(-3deg) }
 }
 #hand-inner{
   transform-origin: 50% 100%;
   &.shake-1,
-  &.shake-2,
-  &.shake-3 {
-    animation: shake-1 .4s infinite alternate linear;
+  &.shake-2 {
+    animation: shake-1 .4s infinite alternate ease-in-out;
   }
-  &.shake-4 {
-    animation: shake-2 .4s infinite alternate linear;
+  &.shake-3 {
+    animation: shake-2 .4s infinite alternate ease-in-out;
   }
 }
 </style>
