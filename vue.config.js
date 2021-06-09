cd97c41444ce72dc047d482c9971885920efdb97
Joaquim Perez <jivkim@gmail.com>
2021-06-10 00:06:49 +0200

    pwa plugin config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/domathome/'
-      : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/domathome/'
+    : '/',
+
+  pwa: {
+    name: 'Dom at Home',
+    themeColor: '#53b45a',
+    appleMobileWebAppCapable: 'yes'
   }
+}
