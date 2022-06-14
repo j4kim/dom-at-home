ec66b9fdf1b3149e63d5f40047ad4c9673715798
Joaquim Perez <jivkim@gmail.com>
2022-06-14 03:20:02 +0200

    keep credits in memory

--- src/store.js
+++ src/store.js
@@ -118,7 +118,7 @@ var store = new Vuex.Store({
 
   mutations: {
     reset (state) {
-      Object.assign(state, initialState())
+      Object.assign(state, initialState(), { credits: state.credits })
       state.sounds.music.rate(1)
     },
     addBodyPart ({ snake }, bodyPart) {
@@ -303,6 +303,7 @@ var store = new Vuex.Store({
       commit("addCredit", 2)
     },
     KeyA ({ state, getters, commit, dispatch }) {
+      console.log("a", getters.canStart, state.over)
       if (getters.canStart) {
         dispatch("startCountdown")
       } else if (state.over) {
